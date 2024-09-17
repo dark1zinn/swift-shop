7e35fbc182ce6fdc06dc4434b64427811e03144b
dark1zinn <edilsonjuininho154@gmail.com>
2024-09-16 23:46:57 -0300

    Deploy configs, + elements change

--- src/elements/Product.jsx
+++ src/elements/Product.jsx
@@ -3,7 +3,13 @@ import './Product.css'
 function Product(props) {
     return(
         <div id="products">
-            <img src="{image-link}" alt="product" />
+            <img src="https://github.com/dark1zinn.png" alt="product" />
+            <br />
+            <h3>This is the title</h3>
+            <p>this is the description</p>
+            <br /><br />
+            <span>this is the product id</span>
+            <button>buy/order button</button>
         </div>
     )
 }
